10ef5dcbcdd34d1587df43ec5ee63cf496307785
psh9321 <parksuhyun9321@gmail.com>
2026-09-24 20:34:52 +0900

    누락 파일 추가

--- src/widgets/CultureInfoDetailPrefetchComponent/components/CultureInfoDetailPageView/_html.tsx
+++ src/widgets/CultureInfoDetailPrefetchComponent/components/CultureInfoDetailPageView/_html.tsx
@@ -29,12 +29,6 @@ export const Wrapper = styled.div`
 
 export const ContentsBox = styled.main`
     width : 600px;
-    max-height : 500px;
-    overflow-y : auto;
-
-    &::-webkit-scrollbar { 
-        display: none;
-    }
 
     @media all and (max-width : 1020px) {
         width : 450px;
@@ -45,7 +39,45 @@ export const ContentsBox = styled.main`
     }
 `
 
+export const ScrollContainer = styled.div`
+    width : 100%;
+    max-height : 500px;
+    overflow-y : auto;
+
+    &::-webkit-scrollbar { 
+        display: none;
+    }
+`
+
 export const TabContents = styled.section`
     margin-top : 20px;
     padding : 20px;
+`
+
+export const TabList = styled.ul`
+    display : flex;
+    background-color : #2d3243;
+    li {
+        button {
+            position : relative;
+            width : 150px;
+            height : 50px;
+            line-height : 50px; 
+
+            &.active {
+                &:after {
+                    content : "";
+                    position : absolute;
+                    bottom : 0;
+                    left : 50%;
+                    transform : translateX(-50%);
+                    display: block;
+                    width : 90%;
+                    height : 5px;
+                    background-color : #7e4ed5;
+                    border-radius : 10px
+                }
+            }
+        }
+    }
 `
